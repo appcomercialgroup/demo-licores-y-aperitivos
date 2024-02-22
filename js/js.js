8390c5ba89579b4c5e5445132473b851f33311d8
Carlos Aleman <caleman9791@gmail.com>
2024-02-22 08:33:11 -0400

    Confirmación de imágenes y texto

---
+++ js/js.js
@@ -0,0 +1,14 @@
+// Creamos un nuevo objeto Date, que representará la fecha actual
+var fechaActual = new Date();
+
+// Array con los nombres de los meses
+var nombresMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// Obtenemos el índice del mes actual
+var indiceMesActual = fechaActual.getMonth();
+
+// Obtenemos el nombre del mes actual utilizando el array de nombres de meses
+var nombreMesActual = nombresMeses[indiceMesActual];
+
+let txtMesActual = document.querySelector(".mesactual");
+txtMesActual.textContent = nombreMesActual;
