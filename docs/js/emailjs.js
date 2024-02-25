ea2a96d53954450808c5cc00c923939e59e90132
Carlos Alemán <caleman9791@gmail.com>
2024-02-25 00:33:55 -0400

    ok

--- docs/js/emailjs.js
+++ docs/js/emailjs.js
@@ -1,28 +1,28 @@
-function enviaCorreo(to_name, message, to_mail) {
+// function enviaCorreo(to_name, message, to_mail) {
 
-	emailjs.send("service_9luz9da", "template_b3mgf3s", {
-		from_name: "Carlos Alemán",
-		to_name: to_name,
-		message: message,
-		to_mail: to_mail,
-		from_mail: "caleman9791@gmail.com",
-	});
+// 	emailjs.send("service_9luz9da", "template_b3mgf3s", {
+// 		from_name: "Carlos Alemán",
+// 		to_name: to_name,
+// 		message: message,
+// 		to_mail: to_mail,
+// 		from_mail: "caleman9791@gmail.com",
+// 	});
 
-}
+// }
 
-document.getElementById('registrationForm')
-	.addEventListener('submit', function (event) {
-		event.preventDefault();
-		console.log('Previene acción por defecto ');
+// document.getElementById('registrationForm')
+// 	.addEventListener('submit', function (event) {
+// 		event.preventDefault();
+// 		console.log('Previene acción por defecto ');
 
-		let txt_name = document.getElementById("contact-name");
-		let txt_email = document.getElementById("contact-email");
-		let txt_message = document.getElementById("contact-message");
+// 		let txt_name = document.getElementById("contact-name");
+// 		let txt_email = document.getElementById("contact-email");
+// 		let txt_message = document.getElementById("contact-message");
 
-		let to_name = txt_name.value;
-		let to_mail = txt_email.value;
-		let message = txt_message.value.trim();
+// 		let to_name = txt_name.value;
+// 		let to_mail = txt_email.value;
+// 		let message = txt_message.value.trim();
 
-		enviaCorreo(to_name, message, to_mail);
+// 		enviaCorreo(to_name, message, to_mail);
 
-	});
+// 	});
