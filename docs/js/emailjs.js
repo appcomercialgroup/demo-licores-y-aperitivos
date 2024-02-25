5344aee08093ccae3e90e0f315789eb5f1e3e6f7
Carlos Alemán <caleman9791@gmail.com>
2024-02-25 00:22:44 -0400

    Actualización de la web

---
+++ docs/js/emailjs.js
@@ -0,0 +1,28 @@
+function enviaCorreo(to_name, message, to_mail) {
+
+	emailjs.send("service_9luz9da", "template_b3mgf3s", {
+		from_name: "Carlos Alemán",
+		to_name: to_name,
+		message: message,
+		to_mail: to_mail,
+		from_mail: "caleman9791@gmail.com",
+	});
+
+}
+
+document.getElementById('registrationForm')
+	.addEventListener('submit', function (event) {
+		event.preventDefault();
+		console.log('Previene acción por defecto ');
+
+		let txt_name = document.getElementById("contact-name");
+		let txt_email = document.getElementById("contact-email");
+		let txt_message = document.getElementById("contact-message");
+
+		let to_name = txt_name.value;
+		let to_mail = txt_email.value;
+		let message = txt_message.value.trim();
+
+		enviaCorreo(to_name, message, to_mail);
+
+	});
